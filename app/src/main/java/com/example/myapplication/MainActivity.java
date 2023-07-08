f260e506a5ee14e8541bde0418a2b3f8b3bd1d13
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-07-08 12:28:50 +0700

    Login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharePreference=new SharePreference(this);
+        sharePreference = new SharePreference(this);
+
+        if (sharePreference.getIslogin()) {
+
+            startHomeUi();
+        } else  {
+
+            etUsername = findViewById(R.id.Username);
+            etPassword = findViewById(R.id.password);
+            btnLogin = findViewById(R.id.btnLogin);
+            pbLogin = findViewById(R.id.pblogin);
+            ivLogin = findViewById(R.id.Login)
+        }
+    }
+
+    public void startHomeUi() {
+        Intent i = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(i);
+        finishAffinity();
+    }
+
+    public void login() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                pbLogin.setVisibility(View.VISIBLE);
+                ivLogin.setVisibility(View.VISIBLE);
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
